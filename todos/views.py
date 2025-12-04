fe3e73f2f1612ff436f7af236d04c057a20363b1
ebtesamqadhi <ebtesamqadhi@gmail.com>
2025-12-04 08:38:56 +0300

    add task_get view and urls

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response 
+from .models import Task
+from .serializer import TaskSerializer
 # Create your views here.
+
+@api_view(['GET'])
+def task_get(request):
+    tasks = Task.objects.all()
+    taskserializer = TaskSerializer(tasks, many=True)
+    data = {
+        "count": tasks.count(),
+        "tasks": taskserializer.data
+    }
+    return Response(data)
