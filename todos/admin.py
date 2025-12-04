9310f32ccb7df5ad94231770248f40beef8c3a32
ebtesamqadhi <ebtesamqadhi@gmail.com>
2025-12-04 07:58:22 +0300

    add TaskAdmin to Django Admin

--- todos/admin.py
+++ todos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from .models import Task
 
 # Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(ModelAdmin):
+    list_display = ["title", "completed", "created_at"]
