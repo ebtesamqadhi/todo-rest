8cb4406981713a027cc94b390efee632fafcfcb3
ebtesamqadhi <ebtesamqadhi@gmail.com>
2025-12-04 16:49:31 +0300

    add task post

--- todos/views.py
+++ todos/views.py
@@ -5,12 +5,20 @@ from .models import Task
 from .serializer import TaskSerializer
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def task_get(request):
-    tasks = Task.objects.all()
-    taskserializer = TaskSerializer(tasks, many=True)
-    data = {
-        "count": tasks.count(),
-        "tasks": taskserializer.data
-    }
-    return Response(data)
+    if request.method == 'POST':
+        taskserializer = TaskSerializer(data = request.data)
+        if taskserializer.is_valid():
+            taskserializer.save()
+            return Response(taskserializer.data, status=201) 
+        return Response(taskserializer.errors, status=400)
+               
+    elif request.method == 'GET':
+        tasks = Task.objects.all()
+        taskserializer = TaskSerializer(tasks, many=True)
+        data = {
+            "count": tasks.count(),
+            "tasks": taskserializer.data
+        }
+        return Response(data, status=200)
